deb1d59193a8c639b332c2d68f4b42f050468cb1
xtremewebapps <xtremewebapps@yahoo.com>
2013-08-04 01:58:06 +0530

    apache tiles 2 implemented

---
+++ mybank/target/tomcat/work/localEngine/localhost/mybank/org/apache/jsp/WEB_002dINF/views/jsp/layout_jsp.java
@@ -0,0 +1,183 @@
+package org.apache.jsp.WEB_002dINF.views.jsp;
+
+import javax.servlet.*;
+import javax.servlet.http.*;
+import javax.servlet.jsp.*;
+
+public final class layout_jsp extends org.apache.jasper.runtime.HttpJspBase
+    implements org.apache.jasper.runtime.JspSourceDependent {
+
+  private static final JspFactory _jspxFactory = JspFactory.getDefaultFactory();
+
+  private static java.util.List _jspx_dependants;
+
+  private javax.el.ExpressionFactory _el_expressionfactory;
+  private org.apache.AnnotationProcessor _jsp_annotationprocessor;
+
+  public Object getDependants() {
+    return _jspx_dependants;
+  }
+
+  public void _jspInit() {
+    _el_expressionfactory = _jspxFactory.getJspApplicationContext(getServletConfig().getServletContext()).getExpressionFactory();
+    _jsp_annotationprocessor = (org.apache.AnnotationProcessor) getServletConfig().getServletContext().getAttribute(org.apache.AnnotationProcessor.class.getName());
+  }
+
+  public void _jspDestroy() {
+  }
+
+  public void _jspService(HttpServletRequest request, HttpServletResponse response)
+        throws java.io.IOException, ServletException {
+
+    PageContext pageContext = null;
+    HttpSession session = null;
+    ServletContext application = null;
+    ServletConfig config = null;
+    JspWriter out = null;
+    Object page = this;
+    JspWriter _jspx_out = null;
+    PageContext _jspx_page_context = null;
+
+
+    try {
+      response.setContentType("text/html; charset=ISO-8859-1");
+      pageContext = _jspxFactory.getPageContext(this, request, response,
+      			null, true, 8192, true);
+      _jspx_page_context = pageContext;
+      application = pageContext.getServletContext();
+      config = pageContext.getServletConfig();
+      session = pageContext.getSession();
+      out = pageContext.getOut();
+      _jspx_out = out;
+
+      out.write("\r\n");
+      out.write("\r\n");
+      out.write("<!DOCTYPE html PUBLIC \"-//W3C//DTD HTML 4.01 Transitional//EN\" \"http://www.w3.org/TR/html4/loose.dtd\">\r\n");
+      out.write("<html>\r\n");
+      out.write("<head>\r\n");
+      out.write("<meta http-equiv=\"Content-Type\" content=\"text/html; charset=ISO-8859-1\">\r\n");
+      out.write("<title>");
+      if (_jspx_meth_tiles_005finsertAttribute_005f0(_jspx_page_context))
+        return;
+      out.write("</title>\r\n");
+      out.write("</head>\r\n");
+      out.write("<body>\r\n");
+      out.write("\t<table border=\"1\" cellpadding=\"2\" align=\"center\"\r\n");
+      out.write("\t\twidth=\"100%\">\r\n");
+      out.write("\t\t<tr\r\n");
+      out.write("\t\t\tstyle=\"background-color: #3D0C02; color: white; text-align: center;\">\r\n");
+      out.write("\t\t\t<td>");
+      if (_jspx_meth_tiles_005finsertAttribute_005f1(_jspx_page_context))
+        return;
+      out.write("</td>\r\n");
+      out.write("\t\t</tr>\r\n");
+      out.write("\t\t<tr style=\"background-color: #8B6D67\">\r\n");
+      out.write("\t\t\t<td height=\"10px\">");
+      if (_jspx_meth_tiles_005finsertAttribute_005f2(_jspx_page_context))
+        return;
+      out.write("</td>\r\n");
+      out.write("\t\t</tr>\r\n");
+      out.write("\t\t<tr>\r\n");
+      out.write("\t\t\t<td height=\"425px\">");
+      if (_jspx_meth_tiles_005finsertAttribute_005f3(_jspx_page_context))
+        return;
+      out.write("</td>\r\n");
+      out.write("\t\t</tr>\r\n");
+      out.write("\t\t<tr>\r\n");
+      out.write("\t\t\t<td height=\"10px\" style=\"background-color: #8B6D67\">");
+      if (_jspx_meth_tiles_005finsertAttribute_005f4(_jspx_page_context))
+        return;
+      out.write("</td>\r\n");
+      out.write("\t\t</tr>\r\n");
+      out.write("\t</table>\r\n");
+      out.write("</body>\r\n");
+      out.write("</html>");
+    } catch (Throwable t) {
+      if (!(t instanceof SkipPageException)){
+        out = _jspx_out;
+        if (out != null && out.getBufferSize() != 0)
+          try { out.clearBuffer(); } catch (java.io.IOException e) {}
+        if (_jspx_page_context != null) _jspx_page_context.handlePageException(t);
+      }
+    } finally {
+      _jspxFactory.releasePageContext(_jspx_page_context);
+    }
+  }
+
+  private boolean _jspx_meth_tiles_005finsertAttribute_005f0(PageContext _jspx_page_context)
+          throws Throwable {
+    PageContext pageContext = _jspx_page_context;
+    JspWriter out = _jspx_page_context.getOut();
+    //  tiles:insertAttribute
+    org.apache.tiles.jsp.taglib.InsertAttributeTag _jspx_th_tiles_005finsertAttribute_005f0 = new org.apache.tiles.jsp.taglib.InsertAttributeTag();
+    org.apache.jasper.runtime.AnnotationHelper.postConstruct(_jsp_annotationprocessor, _jspx_th_tiles_005finsertAttribute_005f0);
+    _jspx_th_tiles_005finsertAttribute_005f0.setJspContext(_jspx_page_context);
+    // /WEB-INF/views/jsp/layout.jsp(8,7) name = name type = null reqTime = true required = false fragment = false deferredValue = false expectedTypeName = null deferredMethod = false methodSignature = null
+    _jspx_th_tiles_005finsertAttribute_005f0.setName("title");
+    // /WEB-INF/views/jsp/layout.jsp(8,7) name = ignore type = boolean reqTime = true required = false fragment = false deferredValue = false expectedTypeName = null deferredMethod = false methodSignature = null
+    _jspx_th_tiles_005finsertAttribute_005f0.setIgnore(true);
+    _jspx_th_tiles_005finsertAttribute_005f0.doTag();
+    org.apache.jasper.runtime.AnnotationHelper.preDestroy(_jsp_annotationprocessor, _jspx_th_tiles_005finsertAttribute_005f0);
+    return false;
+  }
+
+  private boolean _jspx_meth_tiles_005finsertAttribute_005f1(PageContext _jspx_page_context)
+          throws Throwable {
+    PageContext pageContext = _jspx_page_context;
+    JspWriter out = _jspx_page_context.getOut();
+    //  tiles:insertAttribute
+    org.apache.tiles.jsp.taglib.InsertAttributeTag _jspx_th_tiles_005finsertAttribute_005f1 = new org.apache.tiles.jsp.taglib.InsertAttributeTag();
+    org.apache.jasper.runtime.AnnotationHelper.postConstruct(_jsp_annotationprocessor, _jspx_th_tiles_005finsertAttribute_005f1);
+    _jspx_th_tiles_005finsertAttribute_005f1.setJspContext(_jspx_page_context);
+    // /WEB-INF/views/jsp/layout.jsp(15,7) name = name type = null reqTime = true required = false fragment = false deferredValue = false expectedTypeName = null deferredMethod = false methodSignature = null
+    _jspx_th_tiles_005finsertAttribute_005f1.setName("header");
+    _jspx_th_tiles_005finsertAttribute_005f1.doTag();
+    org.apache.jasper.runtime.AnnotationHelper.preDestroy(_jsp_annotationprocessor, _jspx_th_tiles_005finsertAttribute_005f1);
+    return false;
+  }
+
+  private boolean _jspx_meth_tiles_005finsertAttribute_005f2(PageContext _jspx_page_context)
+          throws Throwable {
+    PageContext pageContext = _jspx_page_context;
+    JspWriter out = _jspx_page_context.getOut();
+    //  tiles:insertAttribute
+    org.apache.tiles.jsp.taglib.InsertAttributeTag _jspx_th_tiles_005finsertAttribute_005f2 = new org.apache.tiles.jsp.taglib.InsertAttributeTag();
+    org.apache.jasper.runtime.AnnotationHelper.postConstruct(_jsp_annotationprocessor, _jspx_th_tiles_005finsertAttribute_005f2);
+    _jspx_th_tiles_005finsertAttribute_005f2.setJspContext(_jspx_page_context);
+    // /WEB-INF/views/jsp/layout.jsp(18,21) name = name type = null reqTime = true required = false fragment = false deferredValue = false expectedTypeName = null deferredMethod = false methodSignature = null
+    _jspx_th_tiles_005finsertAttribute_005f2.setName("menu");
+    _jspx_th_tiles_005finsertAttribute_005f2.doTag();
+    org.apache.jasper.runtime.AnnotationHelper.preDestroy(_jsp_annotationprocessor, _jspx_th_tiles_005finsertAttribute_005f2);
+    return false;
+  }
+
+  private boolean _jspx_meth_tiles_005finsertAttribute_005f3(PageContext _jspx_page_context)
+          throws Throwable {
+    PageContext pageContext = _jspx_page_context;
+    JspWriter out = _jspx_page_context.getOut();
+    //  tiles:insertAttribute
+    org.apache.tiles.jsp.taglib.InsertAttributeTag _jspx_th_tiles_005finsertAttribute_005f3 = new org.apache.tiles.jsp.taglib.InsertAttributeTag();
+    org.apache.jasper.runtime.AnnotationHelper.postConstruct(_jsp_annotationprocessor, _jspx_th_tiles_005finsertAttribute_005f3);
+    _jspx_th_tiles_005finsertAttribute_005f3.setJspContext(_jspx_page_context);
+    // /WEB-INF/views/jsp/layout.jsp(21,22) name = name type = null reqTime = true required = false fragment = false deferredValue = false expectedTypeName = null deferredMethod = false methodSignature = null
+    _jspx_th_tiles_005finsertAttribute_005f3.setName("body");
+    _jspx_th_tiles_005finsertAttribute_005f3.doTag();
+    org.apache.jasper.runtime.AnnotationHelper.preDestroy(_jsp_annotationprocessor, _jspx_th_tiles_005finsertAttribute_005f3);
+    return false;
+  }
+
+  private boolean _jspx_meth_tiles_005finsertAttribute_005f4(PageContext _jspx_page_context)
+          throws Throwable {
+    PageContext pageContext = _jspx_page_context;
+    JspWriter out = _jspx_page_context.getOut();
+    //  tiles:insertAttribute
+    org.apache.tiles.jsp.taglib.InsertAttributeTag _jspx_th_tiles_005finsertAttribute_005f4 = new org.apache.tiles.jsp.taglib.InsertAttributeTag();
+    org.apache.jasper.runtime.AnnotationHelper.postConstruct(_jsp_annotationprocessor, _jspx_th_tiles_005finsertAttribute_005f4);
+    _jspx_th_tiles_005finsertAttribute_005f4.setJspContext(_jspx_page_context);
+    // /WEB-INF/views/jsp/layout.jsp(24,55) name = name type = null reqTime = true required = false fragment = false deferredValue = false expectedTypeName = null deferredMethod = false methodSignature = null
+    _jspx_th_tiles_005finsertAttribute_005f4.setName("footer");
+    _jspx_th_tiles_005finsertAttribute_005f4.doTag();
+    org.apache.jasper.runtime.AnnotationHelper.preDestroy(_jsp_annotationprocessor, _jspx_th_tiles_005finsertAttribute_005f4);
+    return false;
+  }
+}
